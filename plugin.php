093200377bb901f900a6a6def800717738fb2a7b
PHP ARTISAN <phprtsan@gmail.com>
2024-01-27 20:23:07 +0600

    removed all defaults

--- plugin.php
+++ plugin.php
@@ -4,22 +4,21 @@
  * Your Plugin Name
  *
  * @package           PluginPackage
- * @author            Your Name
+ * @author            PHP Artisan
  * @license           GPL-2.0-or-later
  *
  * @wordpress-plugin
  * Plugin Name:       Your Plugin Name
- * Plugin URI:        https://example.com/your-plugin-name
+ * Plugin URI:        https://phprtsan.com/your-plugin-name
  * Description:       Description of the plugin.
  * Version:           1.0.0
  * Requires at least: 5.3
  * Requires PHP:      7.4
- * Author:            Your Name
- * Author URI:        https://example.com
+ * Author:            PHP Artisan
+ * Author URI:        https://phprtsan.com
  * Text Domain:       your-plugin-name
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Update URI:        https://example.com/your-plugin-name/
  */
 
 /*
@@ -33,7 +32,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 /*
 |--------------------------------------------------------------------------
-| Load class autoloader
+| Load class autoload
 |--------------------------------------------------------------------------
 */
 require_once __DIR__ . '/vendor/autoload.php';
@@ -66,5 +65,4 @@ try {
 	\PluginPackage\Plugin::init();
 } catch ( Exception $e ) {
 	\PluginPackage\Controllers\LogController::instance()->write( 'error', $e->getMessage() );
-	// throw $e;
 }
